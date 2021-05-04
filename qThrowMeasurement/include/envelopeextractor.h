e6e8388332f771ab07d314a9b37eae0d1a0c9c85
evaristelegallois <70635437+evaristelegallois@users.noreply.github.com>
2021-05-04 16:56:16 +0200

    copied envelopeextractor; to do: proper import

---
+++ qThrowMeasurement/include/envelopeextractor.h
@@ -0,0 +1,11 @@
+#ifndef ENVELOPEEXTRACTOR_H
+#define ENVELOPEEXTRACTOR_H
+
+
+class envelopeExtractor
+{
+public:
+    envelopeExtractor();
+};
+
+#endif // ENVELOPEEXTRACTOR_H
---
+++ qThrowMeasurement/src/envelopeextractor.cpp
@@ -0,0 +1,6 @@
+#include "envelopeextractor.h"
+
+envelopeExtractor::envelopeExtractor()
+{
+
+}
